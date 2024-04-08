13891b014fdfc63bb7b159b08f65ebf7f6265be7
Subhajit Kundu <subha60kundu@gmail.com>
2024-04-08 12:47:19 +0530

    fix: Fixing logout handler typeerror

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,7 @@ import {NextApiRequest, NextApiResponse} from 'next'
 import backend from '@/lib/backend/backend'
 import {redirect} from 'next/navigation'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
 	await backend.auth.logout()
 	redirect('/auth/sign-up')
 }
